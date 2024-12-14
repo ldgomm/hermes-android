13de14f1e0d42f8967e0588d8233d285b882f01a
Jose Ruiz <ruiz211@protonmail.com>
2024-12-13 19:57:57 -0500

    Update Hermes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "com.premierdarkcoffee.sales.hermes"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
