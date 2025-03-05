be5e8e40764ba0d39c4c5dbf3f9dceb210362d64
Jose Ruiz <ruiz211@protonmail.com>
2025-03-05 18:03:06 -0500

    Hermes update

--- app/src/main/java/com/premierdarkcoffee/sales/hermes/root/navigation/route/SettingsRoute.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/hermes/root/navigation/route/SettingsRoute.kt
@@ -1,13 +1,13 @@
 package com.premierdarkcoffee.sales.hermes.root.navigation.route
 
-import androidx.compose.material3.Text
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.premierdarkcoffee.sales.hermes.root.feature.chat.presentation.view.settings.SettingsView
 import com.premierdarkcoffee.sales.hermes.root.navigation.SettingsRoute
 
-fun NavGraphBuilder.settingsRoute() {
+fun NavGraphBuilder.settingsRoute(onUserSignedOutActionTriggered: () -> Unit) {
 
     composable<SettingsRoute> {
-        Text("Pending")
+        SettingsView(onUserSignedOutActionTriggered)
     }
 }
