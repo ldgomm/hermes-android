c69f12718ad368bb39673442614dab38dc4e0210
Jose Ruiz <ruiz211@protonmail.com>
2025-03-13 19:27:26 -0500

    Hermes update

--- app/src/main/java/com/premierdarkcoffee/sales/hermes/root/util/function/getZoomLevel.kt
+++ app/src/main/java/com/premierdarkcoffee/sales/hermes/root/util/function/getZoomLevel.kt
@@ -10,11 +10,8 @@ package com.premierdarkcoffee.sales.hermes.root.util.function
 import kotlin.math.ln
 
 fun getZoomLevel(distance: Int): Float {
-    // Convert distance from km to meters
     val radiusInMeters = distance * 1000.0
-    // Calculate the scale factor for zoom level
     val scale = radiusInMeters / 500.0
-    // Calculate the zoom level based on the scale
     val zoomLevel = 16 - ln(scale) / ln(2.0)
-    return zoomLevel.toFloat().coerceIn(1f, 16f)
+    return zoomLevel.toFloat().coerceIn(1f, 20f)
 }
