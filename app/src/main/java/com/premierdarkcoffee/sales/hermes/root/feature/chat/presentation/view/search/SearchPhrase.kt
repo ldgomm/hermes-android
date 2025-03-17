cc6232ad1bd6b396c3305cc6b4600dd59a3b42da
Jose Ruiz <ruiz211@protonmail.com>
2025-03-17 13:19:01 -0500

    Hermes update

---
+++ app/src/main/java/com/premierdarkcoffee/sales/hermes/root/feature/chat/presentation/view/search/SearchPhrase.kt
@@ -0,0 +1,3 @@
+package com.premierdarkcoffee.sales.hermes.root.feature.chat.presentation.view.search
+
+data class SearchPhrase(val id: String, val iOSIcon: String, val androidIcon: String, val text: String)
